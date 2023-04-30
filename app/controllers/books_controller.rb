7d6a91c620d84b86dd9f16a27b15de9590933a73
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-04-30 11:05:30 +0000

    [投稿機能を実装しました]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,33 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
